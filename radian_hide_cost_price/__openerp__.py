eeae978e86dba991d26df2b0caa158803b0cd8cf
radiandelta <radiancesoft.sby@hotmail.com>
2016-08-21 14:39:33 +0700

    Add files via upload

---
+++ radian_hide_cost_price/__openerp__.py
@@ -0,0 +1,16 @@
+{
+	"name": "Menyembunyikan Cost Price",
+	"version": "1.0.0",
+	"author": "Radian Delta",
+	"depends": ["stock_account"],
+	"category": "",
+	"description": """
+	Menyembunyikan Cost Price.
+	""",
+	"website": "http://www.radiandelta.com",
+	"data": [
+		"product_view.xml",
+    ],
+	"installable": True,
+	"active": False,
+}
---
+++ radian_hide_cost_price/product_template.py
@@ -0,0 +1,4 @@
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+class product_template(osv.osv):
+    _inherit = "product.template"
